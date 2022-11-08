3ebce6966a228eee0e1ec0ed6d5b6cd0f297f179
student <student@localhost>
2022-11-08 18:03:07 +0300

    if/else

--- Start/Program.cs
+++ Start/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Start
+{ 
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());// трехзначное число
+            if(a % 2 != 0)
+            {
+                // сумму цифр 1 + 2 + 3
+                // 123 % 10 12 3 
+                // 587
+                // 364
+            }
+            else
+            {
+                // произведение  цифр  1 * 2 * 3
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
+
