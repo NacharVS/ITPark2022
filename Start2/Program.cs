ff6746716add307f42038bd0175ca215ca0f50d0
student <student@localhost>
2022-12-01 16:22:17 +0300

    new WEapons

--- Start2/Program.cs
+++ Start2/Program.cs
@@ -1,130 +1,17 @@
-﻿using System;
+﻿using Start2.Weapons;
+using System;
 
 
 namespace Start2
 {
     class Program
     {
-        static void Sum()
-        {
-            Console.WriteLine("Не передали аргументы");
-        }
-
-        static void Sum(int a)
-        {
-            Console.WriteLine(a);
-        }
-
-        static void Sum(int a, int b)
-        {
-            Console.WriteLine(a + b);
-        }
-
-        static void Sum(int a, int b, int c)
-        {
-            Console.WriteLine(a + b + c);
-        }
-
-        static void Max(int a, int b, int c)
-        {
-            if(a > b)
-            {
-                if (a > c)
-                {
-                    Console.WriteLine(a);
-                }
-                else
-                {
-                    Console.WriteLine(c);
-                }
-            }
-            else
-            {
-                if(b > c)
-                {
-                    Console.WriteLine(b);
-                }
-                else
-                {
-                    Console.WriteLine(c);
-                }
-            }
-        }
-
-        //static void MultiplyTable()
-        //{
-        //    for (int i = 1; i < 10; i++)
-        //    {
-        //        for (int j = 1; j < 10; j++)
-        //        {
-        //            Console.WriteLine($"{i} * {j} = {i * j}");
-        //        }
-        //        Console.WriteLine();
-        //    }
-        //}
         static void Main()
         {
-            HomeWork.ArrayMaxFor();
-            
-            //int[] array = new int[20];
-            //Random random = new Random();
-
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    array[i] = random.Next(1, 1000);
-
-            //}
-
-            //foreach (var item in array)
-            //{
-            //    Console.Write($"{item} ");
-            //}
-
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    if (array[i] % 7 == 0)
-            //    {
-            //        array[i] = 0;
-            //    }
-            //}
-            //Console.WriteLine();
-            //foreach (var item in array)
-            //{
-            //    Console.Write($"{item} ");
-            //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    peasant.InflictDamage(peasant2);
-            //    peasant2.ShowInfo();
-            //}
-
-
-            //peasant.ShowInfo();
-            //peasant4.ShowInfo();
-            //peasant2.ShowInfo();
-
-            //Max(5464, 78, 1323);
-            //Sum3(132, 5, 5);
+            Pistol pistol = new Pistol(20, 5, "BlackMetal", 0.7, true);
+            pistol.Info();
+            pistol.BurstShoot();
+            pistol.Info();
 
         }
     }
---
+++ Start2/Weapons/Pistol.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start2.Weapons
+{
+    internal class Pistol
+    {
+
+        public int damage;
+        public int rateOfFire;
+        public string color;
+        public double weight;
+        public int clip;
+        public bool canBurstShooting;
+
+        public Pistol(int damage, int rateOfFire, string color, double weight, bool canBurstShooting)
+        {
+            this.damage = damage;
+            this.rateOfFire = rateOfFire;
+            this.color = color;
+            this.weight = weight;
+            clip = 20;
+            this.canBurstShooting = canBurstShooting;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Weapon - Pistol. Damage {damage} Clip: {clip}");
+        }
+        public void Shoot()
+        {
+            Console.WriteLine($"Piu! Damage {damage}");
+            clip--;
+        }
+
+        public void BurstShoot()
+        {
+            if (canBurstShooting)
+            {
+                Shoot();
+                Shoot();
+                Shoot();
+            }
+            else
+                Shoot();
+        }
+    }
+}
