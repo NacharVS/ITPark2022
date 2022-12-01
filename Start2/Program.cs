173018fce095a7343d59ccc80e761ebd5f9c9a09
student <student@localhost>
2022-12-01 16:23:37 +0300

    fix

--- Start2/Program.cs
+++ Start2/Program.cs
@@ -8,7 +8,7 @@ namespace Start2
     {
         static void Main()
         {
-            Pistol pistol = new Pistol(20, 5, "BlackMetal", 0.7, true);
+            Pistol pistol = new Pistol(20, 5, "BlackMetal", 0.7, false);
             pistol.Info();
             pistol.BurstShoot();
             pistol.Info();
